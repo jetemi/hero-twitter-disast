95a4188cb9290b45ed3a4b49a51c75840537fc94
Jetemi <jetemi95@hotmail.com>
2021-11-05 14:15:21 +0100

    settings updated to allo postgres data type

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 TRACK_WORDS = ['Disaster', 'fire']
-TABLE_NAME = "Disaster"
-TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
-            polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
-            user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
-            retweet_count INT, favorite_count INT"
+TABLE_NAME = "Disaster"         #Change DATE,integer, double precision(PostGreSQL) DATETIME,int, double(mySQL)
+TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATE, text VARCHAR(255), \
+            polarity integer, subjectivity integer, user_created_at VARCHAR(255), user_location VARCHAR(255), \
+            user_description VARCHAR(255), user_followers_count integer, longitude double precision, latitude double precision, \
+            retweet_count integer, favorite_count integer"
