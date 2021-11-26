087ecde792c9b8b4b1956ee2542f0551e83c526a
jetemi <jetemi95@hotmail.com>
2021-11-26 15:25:14 +0100

    Update settings.py
    Updating various mentions

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-TRACK_WORDS = ['Disaster', 'fire']
+TRACK_WORDS = ['Disaster', 'COVID-19', 'Ebola', 'Flu', 'outbreaks', 'thunderstorms', 'avalanche', 'severe weather', 'cold wave', 'heatwave', 'snowstorm', 'sandstorm', 'haze', 'risks', 'hazards', 'crisis', 'emergencies', 'bushfire', 'hurricanes', 'fire', 'storms', 'cyclone', 'landslide', 'flood', 'earthquakes', 'wildfire' 'tornadoes', 'drought', 'tsunamis', 'volcanoes', 'blizzard']
 TABLE_NAME = "Disaster"         #Change DATE,integer, double precision(PostGreSQL) DATETIME,int, double(mySQL)
 TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATE, text VARCHAR(255), \
             polarity integer, subjectivity integer, user_created_at VARCHAR(255), user_location VARCHAR(255), \
